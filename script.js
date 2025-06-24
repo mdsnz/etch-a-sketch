ef787eaa1b21e91c2ea4076c0f76d55cfb39b614
mdsnz <maedson001001@gmail.com>
2025-06-24 12:24:16 -0300

    Now the user can chose the display size

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-const containerDiv = document.querySelector('.container-div');
+const changeSizeBtn = document.querySelector(".change-size-btn");
 
-for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-        const pixel = document.createElement('div');
-        pixel.setAttribute('class', 'pixel');
-        containerDiv.appendChild(pixel);
+const sizeText = document.querySelector(".size-text");
+
+const containerDiv = document.querySelector(".container-div");
+
+changeSizeBtn.addEventListener("click", () => {
+  let chosenSize = prompt("Type the new size for the grid (1-100)", 8);
+  sizeText.innerHTML = `Current display size: ${chosenSize}x${chosenSize}`;
+  renderGrid(chosenSize);
+  let correctPixelSize = (500/chosenSize) - 2;
+  console.log(correctPixelSize)
+  changePixelsSize(correctPixelSize);
+});
+
+renderGrid(8);
+
+function renderGrid(size) {
+  containerDiv.innerHTML = ``;
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const pixel = document.createElement("div");
+      pixel.setAttribute("class", "pixel");
+      containerDiv.appendChild(pixel);
     }
+  }
+  const pixels = document.querySelectorAll(".pixel");
+
+  pixels.forEach((pixel) => {
+    pixel.addEventListener("mouseover", () => {
+      pixel.setAttribute("class", "pixel pixel-painted");
+    });
+  });
 }
 
-const pixels = document.querySelectorAll('.pixel');
+function changePixelsSize(correctPixelSize) {
 
-pixels.forEach((pixel) => {
-    pixel.addEventListener('mouseover', () => {
-        pixel.setAttribute('class', 'pixel pixel-painted')
+    const pixels = document.querySelectorAll(".pixel");
+
+    pixels.forEach((pixel) => {
+        pixel.style.width = `${correctPixelSize}px`;
+        pixel.style.height = `${correctPixelSize}px`
     })
-} )
+}
