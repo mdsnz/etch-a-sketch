d7ea4108a44a964ca6866c559bb5beda2673c35e
mdsnz <maedson001001@gmail.com>
2025-06-24 11:43:47 -0300

    Adding text, button and styling

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 const containerDiv = document.querySelector('.container-div');
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
         const pixel = document.createElement('div');
         pixel.setAttribute('class', 'pixel');
         containerDiv.appendChild(pixel);
