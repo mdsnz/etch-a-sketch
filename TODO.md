fcb222e5bc532736a36a1b966f8d08846f616924
mdsnz <maedson001001@gmail.com>
2025-07-11 12:19:08 -0300

    Creating new Todo tracking file

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO List
+
+- [x] Paint square only if click or click and drag
+- [ ] Make the rainbow mode optional
+- [ ] Make the transparency mode an easter egg
+- [ ] Add eraser
+- [ ] Add Clear
+- [ ] Add a box for the user choose the color
+- [ ] Add sizeSlider (<input type="range">) 
--- script.js
+++ script.js
@@ -71,15 +71,3 @@ function changePixelsSize(correctPixelSize) {
     pixel.style.height = `${correctPixelSize}px`;
   });
 }
-
-/* 
-TODO: 
-[x] Paint square only if click or click and drag
-[] Make the rainbow mode optional
-[] Make the transparency mode an easter egg
-[] Add eraser
-[] Add Clear
-[] Add a box for the user choose the color
-[] Add sizeSlider (<input type="range">)
-
-*/
